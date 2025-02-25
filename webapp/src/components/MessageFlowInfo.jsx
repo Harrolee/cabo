6aac8daa384254f3c381a35d2e9409ced879bb83
Lee Harrold <halzinnia@gmail.com>
2025-02-24 19:52:33 -0500

    new phone, who dis?

--- webapp/src/components/MessageFlowInfo.jsx
+++ webapp/src/components/MessageFlowInfo.jsx
@@ -6,7 +6,7 @@ export function MessageFlowInfo() {
       <ul className="list-disc list-inside text-sm text-gray-700 mt-2">
         <li>Service: You'll receive daily CaboFit motivation texts to help you get beach-ready.</li>
         <li>Content: Each text includes a motivational quote and two beach fitness inspiration images.</li>
-        <li>Source: You'll only receive texts from this number: +1 (877)-374-1187</li>
+        <li>Source: You'll only receive texts from this number: +1 (317)-785-1183</li>
         <li>Opt-in: By signing up, you consent to receive messages.</li>
         <li>Opt-in Handshake: You'll receive a confirmation text when you sign up.</li>
         <li>Terms: Reply STOP to opt-out, HELP for help.</li>
--- webapp/src/components/MoreInfoSection.jsx
+++ webapp/src/components/MoreInfoSection.jsx
@@ -6,7 +6,7 @@ export function MoreInfoSection() {
       <ul className="list-disc list-inside text-sm text-gray-700 mt-2">
         <li>Service: Users will receive a daily set of workout motivation texts.</li>
         <li>Content: Texts will include a motivational quote and two motivational images.</li>
-        <li>Source: Users will only receive texts from this number: +1 (877)-374-1187</li>
+        <li>Source: Users will only receive texts from this number: +1 (317)-785-1183</li>
         <li>Opt-in: By signing up, you consent to receive messages.</li>
         <li>Opt-in Handshake: When you sign up, you will receive a confirmation text.</li>
         <li>Terms: Reply STOP to opt-out, HELP for help.</li>
